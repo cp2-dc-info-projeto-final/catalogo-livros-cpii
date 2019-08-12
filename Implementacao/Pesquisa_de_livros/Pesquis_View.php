543f8a6b64da1b2a54dea1cd5f02f85ea8841997
lab lab <labcaxias@labdc.zentyal>
2019-08-12 07:18:12 -0300

    Pesquisa de livro inicial

---
+++ Implementacao/Pesquisa_de_livros/Pesquis_View.php
@@ -0,0 +1,11 @@
+<html>
+     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> <!-- tag para colocar adaptar o site para a codificação UTF-8 -->
+   <head>
+       
+   <title>Login</title> <!-- nome na aba da página -->
+      <link rel="stylesheet" type="text/css" href="pesquis_style.css"> <!-- hyperlink para a conexão com o CSS-->
+
+  </head>
+
+
+</html>
