c16ab02725997be50ba8aa07f234304c01c2fef2
lab lab <labcaxias@labdc.zentyal>
2019-08-14 08:45:04 -0300

    Pesquis de livro 3

--- Implementacao/Pesquisa_de_livros/Pesquis_View.php
+++ Implementacao/Pesquisa_de_livros/Pesquis_View.php
@@ -1,7 +1,16 @@
 <html>
      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/> <!-- tag para colocar adaptar o site para a codificação UTF-8 -->
    <head>
-       
+   <script language = "javascript" type=text/javascript">
+       function getInformacaoQualquer(){
+         createRequest();
+         var url = "getInformacaoQualquerDoServidor-ajax.php";
+         request.open("GET", url, true);
+         request.onreadystatechange = atualizaPagina;
+         request.send(null);
+       }
+      </script>
+
    <title>Pesquisa de livros</title> <!-- nome na aba da página -->
       <link rel="stylesheet" type="text/css" href="pesquis_style.css"> <!-- hyperlink para a conexão com o CSS-->
 
