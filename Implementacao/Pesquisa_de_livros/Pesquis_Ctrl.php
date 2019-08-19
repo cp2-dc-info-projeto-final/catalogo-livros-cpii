03021fc54a44854f86c907a437ae2b6a31917ea9
lab lab <labcaxias@labdc.zentyal>
2019-08-19 08:04:22 -0300

    Pesquis 7

--- Implementacao/Pesquisa_de_livros/Pesquis_Ctrl.php
+++ Implementacao/Pesquisa_de_livros/Pesquis_Ctrl.php
@@ -1,3 +1,4 @@
+<?php header('Content-Type': text/html: charset=iso-8859-1); ?> <!--- corrige o problema de acentuação --->
 <?php
 session_start();
  require ("Pesquis_Model.php");
--- Implementacao/Pesquisa_de_livros/Pesquis_Model.php
+++ Implementacao/Pesquisa_de_livros/Pesquis_Model.php
@@ -1,5 +1,26 @@
 <?php
   
-  
+  function buscar(livro){
+    if($conn===false){
+        die("Falha na conexão". mysqli_connect_error());
+        $sql = "SELECT * FROM livro WHERE nome LIKE '%$palavra%'";
+        $query = mysql_query($sql);
+        $qtd = mysql_num_rows($query);
+ }
+    var page = "Pesquis_Ctrl.php"
+    $.ajax
+          ({
+            type: "POST"
+            dataType:"html"
+            url: page
+            beforeSend: function(){
+                        $("#dados").html("Carregando...");
+            },
+            data: (palvra:palavra),
+            success: function(msg)
+            {
+               $("#dados").html(msg);
+            }
+          }); 
 
 ?>
