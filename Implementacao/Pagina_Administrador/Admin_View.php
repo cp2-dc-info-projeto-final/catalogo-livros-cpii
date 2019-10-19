1a2b2cc009a22afcc7928002c87b3a69f6381e8f
Kah750 <kah750@gmail.com>
2019-10-18 23:53:27 -0300

    Página do Administrador - Progresso

---
+++ Implementacao/Pagina_Administrador/Admin_View.php
@@ -0,0 +1,141 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <!-- tag para colocar adaptar o site para a codificação UTF-8 -->
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="AdminStyle.css">
+
+            <title>
+                Página do Administrador
+            </title>
+    </head>
+
+    <body>
+    
+    <table id="TabelaUsuarios">
+  <caption>Usuários</caption>
+  <thead>
+    <tr>
+      
+      <th>Nome</th>
+      <th>Status</th>
+      <th>Ação</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr>
+      
+      <td>Hide You</td>
+      <td>Kosheen</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+      
+      <td>.38.45</td>
+      <td>Thievery Corporation</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+      
+      <td>Fix You</td>
+      <td>Coldplay</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+      
+      <td>Maps</td>
+      <td>Yeah Yeah Yeahs</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+      
+      <td>Ask me how I am</td>
+      <td>Snow Patrol</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+      
+      <td>PMT</td>
+      <td>Deeper Water</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+      
+      <td>Four Kicks</td>
+      <td>Kings of Leon</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+      
+      <td>Gravity</td>
+      <td>Embrace</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+      
+      <td>Lyla</td>
+      <td>Oasis</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+      
+      <td>Look What You've Done</td>
+      <td>Jet</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+      
+      <td>Chicken Payback</td>
+      <td>The Bees</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+      
+      <td>Walkabout</td>
+      <td>Blue States</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+     
+      <td>Oh My God</td>
+      <td>Kaiser Chiefs</td>
+      <td>Tornar Administrador</td>
+    </tr>
+
+    <tr>
+      
+      <td>Rock Scene</td>
+      <td>Athlete</td>
+      <td>Tornar Administrador</td>
+    </tr>
+  </tbody>
+</table>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </body>
+
+
+
+</html>
