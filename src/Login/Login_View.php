fb2c8aeed397c3b5597eaa9334abe2b64bd69eb8
kah750 <kah750@gmail.com>
2019-11-04 15:19:45 -0300

    Atualização - Código

--- src/Login/Login_View.php
+++ src/Login/Login_View.php
@@ -13,6 +13,8 @@
 				<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 				<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
 				<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+				<link rel="stylesheet" href="css/bootstrap.min.css">
+				<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> 
 			<!-- css do menu superior --> -->
 			<link rel="stylesheet" type="text/css" href="css/util.css">
 			<link rel="stylesheet" type="text/css" href="css/main.css">
